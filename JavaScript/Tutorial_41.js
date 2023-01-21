483202484a1b27cdf0c150b8a31a1a7aaf8c218e
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-01-21 16:16:28 +0530

    Operators
    Operators

--- JavaScript/Tutorial_41.js
+++ JavaScript/Tutorial_41.js
@@ -1,1 +1,34 @@
-a
+var a = 10243;
+var b = "Abhinav";
+c = 36.46;
+console.log(a);
+console.log(b);
+// console.log(c);
+
+// Operators in JavaScript
+// Operand - Entities on which operators operate
+// In 3 + 4 => '+' is operator and 3, 4 are operands
+
+// 1. Unary Operator - It has single operand (x = -x)
+// Example of unary operator
+c = -c;
+// console.log(c);
+
+// 2. Binary Operator - It has two operand (x = x + 6)
+// Example of binary operator
+c = 446 + 44;
+// console.log(c);
+
+var num1 = 3;
+var num2 = 4;
+
+// Arithematic operators in action in JavaScript
+console.log("The value of num1 + num2 is: "+ (num1 + num2));
+console.log("The value of num1 - num2 is: "+ (num1 - num2));
+console.log("The value of num1 * num2 is: "+ (num1 * num2));
+console.log("The value of num1 / num2 is: "+ (num1 / num2));
+console.log("The value of num1 ** num2 is: "+ (num1 ** num2));
+console.log("The value of num1 ++ is: "+ (num1 ++));
+console.log("The value of ++ num1 is: "+ (++num1));
+console.log("The value of num1 -- is: "+ (num1 --));
+console.log("The value of -- num1 is: "+ (--num1));
