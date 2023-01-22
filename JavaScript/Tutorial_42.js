871b3e824143b9a0fe534b3872b373713739893c
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-01-22 14:27:11 +0530

    String Methods
    String Methods

--- JavaScript/Tutorial_42.js
+++ JavaScript/Tutorial_42.js
@@ -1,1 +1,20 @@
-!
+// var string = "this";
+var string = 'This is ';
+var name = 'Abhinav';
+var channel = 'CodingPunisher'
+var message = '. Abhinav is a smart boy. ';
+var temp = `${name} is a 'nice' person and he has a "channel" called ${channel}.`;
+console.log(string + name + message);
+// console.log(temp);
+// var len =name.length;
+// console.log(`Length of name is ${len}`);
+
+// Esscape sequence characters - \n, \t, \b
+
+// console.log("Hello World\nAbhinav\tand")
+
+// Constructor
+var y = new String("Code");
+console.log(y);
+
+document.getElementById('content').innerHTML = '<h3>This is a h3 heading</h3>'
