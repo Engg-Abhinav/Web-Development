3d2b62006aa7395274d71017616f6b90d8025893
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-01-22 23:45:45 +0530

    String Function
    String Function

--- JavaScript/Tutorial_43.js
+++ JavaScript/Tutorial_43.js
@@ -1,1 +1,17 @@
-!
+var str = "This is a string";
+console.log(str);
+// indexOf will give us position of first occurance of a substring
+var position = str.indexOf('is');
+console.log(position);
+
+position = str.lastIndexOf('is');
+console.log(position);
+
+// Substring from a string
+// Slice can take negative value but substring cannot take negative value
+
+// var substg = str.slice(1,11);
+// var substrg = str.substring(1,11);
+var substrg = str.substr(1,11);
+console.log(substrg);
+
