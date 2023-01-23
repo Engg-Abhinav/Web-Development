d8233b02d00ecf7bcb7ac956ccc4b114afa4d1d3
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-01-23 23:56:50 +0530

    Arrays and Objects
    Arrays and Objects

--- JavaScript/Tutorial_45.js
+++ JavaScript/Tutorial_45.js
@@ -1,1 +1,25 @@
-!
+let myVar1 = 34;
+let myVar2 = 'String';
+let myVar3 = true;
+let myVar4 = null;
+let myVar5 = undefined;
+
+// Object
+// let employee= {
+//     name:'Abhinav',
+//     salary:1000000,
+//     channel1:'CodingPunisher',
+//     channel2:'ProgrammingPunisher',
+// }
+// console.log(employee);
+
+// Array
+// let arr = [1, 2, 3, 4, 'Abhinav', undefined];
+// console.log(arr[4]);
+
+
+let arr = new Array(9, 7, 3, 4, 'Abhinav', undefined); // This is an object
+console.log(arr);
+console.log(arr.length);
+arr = arr.sort();
+console.log(arr);
