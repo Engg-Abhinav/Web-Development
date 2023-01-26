7552477869296fade6d01c8debd5342f7b3ef72a
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-01-26 23:56:49 +0530

    Update Tutorial_49.js

--- JavaScript/Tutorial_49.js
+++ JavaScript/Tutorial_49.js
@@ -1,1 +1,10 @@
-!
+function toggleHide(){
+    btn = document.getElementById('btn');
+    para = document.getElementById('para');
+    if(para.style.display!='none'){
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
