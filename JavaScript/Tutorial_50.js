73eae36a11bf94c4de5ab6aaf593d5ff638d3efc
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-01-27 23:58:48 +0530

    Update Tutorial_50.js

--- JavaScript/Tutorial_50.js
+++ JavaScript/Tutorial_50.js
@@ -1,1 +1,27 @@
-!
+console.log("This is a JavaScript Tutorial.")
+
+// setTimeout - Allows us to run the dunction once after the interval of time
+// clearTimeout - Allows us to run the function repeatedly after the interval of time
+
+function greet(name, byeText){
+    console.log("Hello, Good Morning "+name+" "+byeText);
+}
+
+// setTimeout(greet,5000, "Abhinav", "Take Care.");
+
+// timeOut = setTimeout(greet,5000, "Abhinav", "Take Care.");
+
+// console.log(timeOut); // This will give some ID
+
+// clearTimeout(timeOut); // Passing that ID into clearTimeout function and this will cancel the schedule
+
+// setInterval(greet, 1000, "Abhinav", "Take care");
+
+// intervalId=setInterval(greet, 1000, "Abhinav", "Take care");
+// clearInterval(intervalId);
+
+function displayTime(){
+    time=new Date();
+    console.log(time);
+    document.getElementById('time').innerHTML = time;
+}
